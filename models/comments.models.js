48b9ee2d21ecda9c69c5c9bfc670d2355dc4362a
Damian <damiangildea12@gmail.com>
2023-06-30 11:54:05 +0100

    Tests and functionality for task 17

--- models/comments.models.js
+++ models/comments.models.js
@@ -10,3 +10,21 @@ exports.deleteCommentById = (comment_id) => {
       return result.rowCount;
     });
 };
+
+exports.updateCommentVotes = (commentId, incVotes) => {
+  if (typeof incVotes !== "number" || incVotes === undefined) {
+    return Promise.reject({ status: 400, msg: "Bad request - Invalid inc_votes value" });
+  }
+
+  return db
+    .query(
+      "UPDATE comments SET votes = votes + $1 WHERE comment_id = $2 RETURNING *",
+      [incVotes, commentId]
+    )
+    .then((result) => {
+      if (result.rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Comment not found" });
+      }
+      return result.rows[0];
+    });
+};
