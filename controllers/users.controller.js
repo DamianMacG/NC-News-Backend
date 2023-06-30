6255ff551b8b8c4bd976dbc7a96bb7f50606f27e
Damian <damiangildea12@gmail.com>
2023-06-30 11:10:00 +0100

    Tests and functionality for task 16

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,9 +1,33 @@
-const { getUsers } = require("../models/users.model");
+const { getUsers, getUsername } = require("../models/users.model");
 
 exports.getAllUsers = (req, res, next) => {
   getUsers()
     .then((users) => {
       res.status(200).send({ users });
+    })
+    .catch(next);
+};
+
+exports.getUserByUsername = (req, res, next) => {
+  const { username } = req.params;
+
+  getUsername(username)
+    .then((user) => {
+      if (typeof user.username !== "string") {
+        return Promise.reject({ status: 400, msg: "Bad Request" });
+      }
+
+      res.status(200).send({ user });
+    })
+    .catch(next);
+};
+
+exports.getUserByUsername = (req, res, next) => {
+  const { username } = req.params;
+
+  getUsername(username)
+    .then((user) => {
+      res.status(200).send({ user });
     })
     .catch(next);
 };
